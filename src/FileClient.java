90ec263bb20d6a48921ed009500a3037096d6672
Zortrox <zortroxofdoom@gmail.com>
2017-03-29 12:18:53 -0500

    some initial queues and code to listen and send

--- src/FileClient.java
+++ src/FileClient.java
@@ -7,13 +7,15 @@ import javax.swing.text.DefaultCaret;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileInputStream;
+import java.nio.file.Path;
+
+import static javax.swing.JFileChooser.APPROVE_OPTION;
 
 public class FileClient {
 
     public static void main(String[] args) {
         NetObject client = new NetObject("File Client");
-
-
     }
 }
--- src/NetObject.java
+++ src/NetObject.java
@@ -4,11 +4,18 @@
 
 import javax.swing.*;
 import javax.swing.text.DefaultCaret;
+import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.file.*;
-import java.util.Arrays;
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+import static javax.swing.JFileChooser.APPROVE_OPTION;
 
 class Message {
 	byte mType = 0;
@@ -21,6 +28,7 @@ public class NetObject {
 	private String filePath;
 
 	//Swing stuff
+	private JFrame frame = null;
 	private JTextArea txtMessages = null;
 	private JScrollPane scrollPane = null;
 
@@ -32,12 +40,20 @@ public class NetObject {
 	private static final byte MSG_TEXT = 1;	//sending text
 	private static final byte MSG_FILE = 2;	//sending file
 
+	//network objects
+	private DatagramSocket listenSocket;
+	private BlockingQueue<DatagramPacket> qPackets = new LinkedBlockingQueue<>();
+	private BlockingQueue<Message> qMessages = new LinkedBlockingQueue<>();
+	private ArrayList<BlockingQueue<Message>> arrReceived = new ArrayList<>();
+	private Semaphore mtxArray = new Semaphore(1);
+	private Semaphore mtxMessages = new Semaphore(1);
+
 	NetObject(String strTitle) {
 		//get relative path
 		filePath = new File("").getAbsolutePath();
 
 		//create window
-		JFrame frame = new JFrame(strTitle);
+		frame = new JFrame(strTitle);
 		frame.setSize(600, 400);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -54,35 +70,96 @@ public class NetObject {
 
 	//receiving data
 	public boolean listen(final int port) {
-		Thread thrListen = new Thread(new Runnable() {
-			@Override
-			public void run() {
+		try {
+			listenSocket = new DatagramSocket(port);
 
-			}
-		});
-		thrListen.start();
+			Thread thrListen = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						System.out.println("<server>: Listening for packets.");
 
-		//process packets
-		Thread thrProcess = new Thread(new Runnable() {
-			@Override
-			public void run() {
+						while (true) {
+							byte[] receiveData = new byte[PACKET_SIZE];
+							DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+							listenSocket.receive(receivePacket);
 
-			}
-		});
-		thrProcess.start();
+							qPackets.put(receivePacket);
+						}
+					} catch (Exception ex) {
+						ex.printStackTrace();
+					}
+				}
+			});
+			thrListen.start();
+
+			//process packets
+			Thread thrProcess = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						while (true) {
+							//receive data
+							DatagramPacket receivePacket = qPackets.take();
+							writeMessage("New Packet");
+							Message msg = new Message();
+							//process packet into the message
+							processUDPData(receivePacket, msg);
+
+							switch (msg.mType) {
+								case MSG_INIT:
+									String strInitial = new String(msg.mData);
+									msg.mData = ("Message received, " +
+											strInitial.substring(strInitial.lastIndexOf(' ') + 1)).getBytes();
+									sendUDPData(listenSocket, msg);
+									break;
+								case MSG_TEXT:
+									String recMsg = new String(msg.mData);
+									msg.mData = recMsg.toUpperCase().getBytes();
+									sendUDPData(listenSocket, msg);
+									break;
+								case MSG_FILE:
+									String filename = new String(msg.mData);
+									//sendFile(listenSocket, msg, filename);
+									System.out.println("Sent: " + filename);
+									break;
+							}
+						}
+					} catch (Exception ex) {
+						ex.printStackTrace();
+					}
+				}
+			});
+			thrProcess.start();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return false;
+		}
 
 		return true;
 	}
 
 	//sending data
-	public boolean connect(String IP, final int port) {
-		Thread thrConnect = new Thread(new Runnable() {
-			@Override
-			public void run() {
+	public boolean connect() {
+		try {
+			DatagramSocket sendSocket = new DatagramSocket();
 
-			}
-		});
-		thrConnect.start();
+			Thread thrConnect = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						Message msg = qMessages.take();
+						sendUDPData(sendSocket, msg);
+					} catch (Exception ex) {
+						ex.printStackTrace();
+					}
+				}
+			});
+			thrConnect.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 
 		return true;
 	}
@@ -132,18 +209,45 @@ public class NetObject {
 		socket.send(sendPacket);
 	}
 
-	public void sendFile(Object socket, Message msg, String filename) {
-		Path file = Paths.get(filePath + filename);
-		try {
-			byte[] fileData = Files.readAllBytes(file);
+	public void sendFile(String IP, int port) {
+		JFileChooser fileChooser = new JFileChooser();
+		int opt = fileChooser.showOpenDialog(frame);
 
-			msg.mType = MSG_FILE;
-			msg.mData = fileData;
+		if (opt == APPROVE_OPTION) {
+			final Path file = fileChooser.getSelectedFile().toPath();
 
-			sendUDPData((DatagramSocket) socket, msg);
-		}
-		catch (Exception ex) {
-			ex.printStackTrace();
+			Thread thrSend = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						Message msg = new Message();
+						msg.mIP = InetAddress.getByName(IP);
+						msg.mPort = port;
+						msg.mType = MSG_FILE;
+
+						byte[] fileData = Files.readAllBytes(file);
+
+						BlockingQueue<Message> queue = new LinkedBlockingQueue<>();
+						mtxArray.acquire();
+						int index = arrReceived.size();
+						arrReceived.add(queue);
+						mtxArray.release();
+
+						DatagramSocket sock = new DatagramSocket();
+
+						//send file start packet
+
+
+						msg.mData = fileData;
+
+						qMessages.put(msg);
+					}
+					catch (Exception ex) {
+						ex.printStackTrace();
+					}
+				}
+			});
+			thrSend.start();
 		}
 	}
 
@@ -162,8 +266,10 @@ public class NetObject {
 
 	public void writeMessage(String msg) {
 		try {
+			mtxMessages.acquire();
 			txtMessages.append(msg + "\n");
 			scrollPane.scrollRectToVisible(txtMessages.getBounds());
+			mtxMessages.release();
 		} catch (Exception e) {
 			//e.printStackTrace();
 		}
